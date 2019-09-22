2789716ba0d06b3367858a3ff5b495fc4fa33c03
beks2995 <52246823+beks2995@users.noreply.github.com>
2019-09-23 05:35:58 +0600

    fix error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+	// Do not use eval!!!
+	return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+	const arrString = expr.trim().split(''),
+		bracketOne = arrString.filter(symbol => symbol === '(').length,
+		bracketTwo = arrString.filter(symbol => symbol ===')').length;
+
+	if (checkLeftBrackets !== checkRightBrackets) {
+		throw new Error ('ExpressionError: Brackets must be paired')
+	}
+	else {
+		let result = (new Function('return ' + expr))();
+		if(result === Infinity) {
+			throw new Error ('TypeError: Devision by zero.')
+		}
+		return result
+	}
 }
 
 module.exports = {
-    expressionCalculator
+	expressionCalculator
 }
